abdfe6c7767059566a5bd8c6cbce75d8e98b4f9a
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2019-07-05 17:26:58 +0200

    Adding POJOs, Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pet {
+	private PetType petType;
+	private Owner owner;
+	private LocalDate birthday;
+}
