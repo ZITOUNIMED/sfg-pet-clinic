ef86167899034ba3c7072bd96c2de4fbd9de5014
ZITOUNI Mohamed <zitouni.mohamed.email@gmail.com>
2019-07-09 13:12:54 +0200

    List Vets in index Page. Closes #14.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class VetsController {
+import guru.springframework.sfgpetclinic.services.VetService;
 
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets(){
+@Controller
+@RequestMapping("/vets")
+public class VetsController {
+	private final VetService vetService;	
+	
+	public VetsController(VetService vetService) {
+		this.vetService = vetService;
+	}
+	@RequestMapping({"", "/index", "/index.html"})
+	public String listVets(Model model){
+		model.addAttribute("vets", vetService.findAll());
 		return "/vets/index";
 	}
 }
